06b71707bf67ef4423bfc70cbf5094ee66443735
Steven Trinh <strinh@gaming.tech>
2025-11-14 13:01:46 +0100

    hu

--- MorpionConsole/MorpionConsole.cpp
+++ MorpionConsole/MorpionConsole.cpp
@@ -1,9 +1,39 @@
 #include <iostream>
 #include <array>
 #include <conio.h>
+#include <Windows.h>
 
 using Board = std::array<char, 9>;
+unsigned int stock;
 int parties = 0;
+
+char GetQuiJoue()
+{
+    if (stock & 0x01)
+    {
+        return 'X';
+    }
+    return 'O';
+}
+
+void SetQuiJoue()
+{
+    // Crée un masque où tous les bits valent 1 sauf le bit de poids faible (bit 0) : 11111110 (sur 8 bits).
+    unsigned int mask = ~1; // interchange les 0 en 1 et les 1 en 0 du bit choisie (~1 <=> 0xFFFFFFFE) sauf le dernier
+    // (stock & mask) garde les ancienne valeur car si c'était 1 1et 1 = 1 et si c'était 0 0et 1 = 0
+    // (~stock & 0x01) iverse tout et garde que le plus a droite car 0x01 et ajoute a stock
+    stock = (stock & mask) | (~stock & 0x01);
+}
+
+BYTE GetEtatPartie()
+{
+    return (stock & 0x06) >> 1; // lit les bit créant la valeur 6 soit en binaire 0110 et >> 1 ramène vers la droite soit 0011
+}
+
+void SetEtatPartie(BYTE etatPartie)
+{
+    stock = stock | etatPartie;
+}
 
 void afficherPlateau(const Board& b)
 {
@@ -87,7 +117,7 @@ int demanderCoup(const Board& b, char joueur)
         int index = choix - 1;
         if (caseLibre(b, index) == false)
         {
-            std::cout << "Case déjà prise, choisissez-en une autre.\n";
+            std::cout << "Case deja prise, choisissez-en une autre.\n";
             continue;
         }
 
@@ -100,7 +130,8 @@ int main()
     Board plateau;
     plateau.fill(' ');
 
-    char joueurCourant = 'X';
+    /*char joueurCourant = 'X';*/
+    char joueurCourant = GetQuiJoue();
     bool fini = false;
 
     while (true)
@@ -115,7 +146,7 @@ int main()
             if (coupGagnant(plateau, joueurCourant))
             {
                 afficherPlateau(plateau);
-                std::cout << "Le joueur " << joueurCourant << " a gagné !\n";
+                std::cout << "Le joueur " << joueurCourant << " a gagne !\n";
                 fini = true;
             }
             else if (plateauPlein(plateau))
@@ -126,7 +157,11 @@ int main()
             }
             else
             {
-                joueurCourant = (joueurCourant == 'X') ? 'O' : 'X';
+                SetQuiJoue(); // change valeur dans stock
+                joueurCourant = GetQuiJoue(); // regarde stock pour connaitre qui vas jouer
+
+
+
             }
         }
         parties++;
